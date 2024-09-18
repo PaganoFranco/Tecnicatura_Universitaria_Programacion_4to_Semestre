1158c61ade7b69967ce801e640027e42fe5a7152
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-09-18 20:23:15 -0300

    Comenzamos con -> Decimal, Binario, Octal y Hexadecimal

---
+++ PYTHON/Clase 02 Profundizando en Python con Sistemas Numéricos/Sistemas_numeracion.py
@@ -0,0 +1,17 @@
+# Profundizando en los sistemas de numeración
+
+# Sistema decimal
+a = 10
+print(f"a decimal: {a}")
+
+# Sistema binario
+a = 0b1010 # El "0b" hace referencia a número binario
+print(f"a binario: {a}")
+
+# Sistema octal
+a = 0o12 # El "0o" hace referencia al sistema octal
+print(f"a octal: {a}")
+
+# Sistema hexadecimal
+a = 0xA # El "0x" hace referencia al sistema hexadecimal
+print(f"a hexadecimal: {a}")
