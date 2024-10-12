566c2486bcb10a9f72ebb0534d51181b21de3b23
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-10-12 17:42:48 -0300

    Abrimos el archivo en Intellij -> recuerden que se descarga comprimido en Winrar

--- JAVA/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ JAVA/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -3,11 +3,12 @@ package utn.estudiantes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //Anotacion para ejecucion
 public class EstudiantesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
 	}
-
+// Tenemos el main con la clase Sprint
+	
 }
