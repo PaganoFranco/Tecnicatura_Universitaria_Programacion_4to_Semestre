8d33034da6bb26093c2d029b8d3e87f8b199b7a2
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-10-12 20:12:51 -0300

    Clase help() - Parte 1 -> Consulta con la clase str (String)

--- PYTHON/Clase 02 Profundizando en Python con Sistemas Numéricos/tipo_str.py
+++ PYTHON/Clase 02 Profundizando en Python con Sistemas Numéricos/tipo_str.py
@@ -5,4 +5,7 @@ variable = " Pagano"
 mensaje = "Hola " + "Franco" # El sigo de mas es innecesario usarlo siempre y cuando sean los dos string o esten estre ""
 mensaje = "Hola ""Franco" + variable # en este caso a juntar con una variable si se utiliza el conectar
 mensaje += ", Terminamos"
-print(mensaje)
+# print(mensaje)
+
+# usamos la clase help que esta incluida en python (built-in), se usa para ayudar o documentar
+help(str)
