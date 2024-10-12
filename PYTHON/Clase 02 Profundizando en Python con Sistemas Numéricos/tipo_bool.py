07929816b35802db5f9aa9cfa52dabcb8947c3c4
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-10-12 19:58:36 -0300

    Parte 3 -> Trabajamos con las Colecciones

--- PYTHON/Clase 02 Profundizando en Python con Sistemas Numéricos/tipo_bool.py
+++ PYTHON/Clase 02 Profundizando en Python con Sistemas Numéricos/tipo_bool.py
@@ -17,3 +17,33 @@ print(f"valor: {valor}, Resultado: {resultado}")
 valor = ""
 resultado = bool(valor)
 print(f"valor: {valor}, Resultado: {resultado}")
+
+# Tipo colecciones -> False para colecciones vacias
+# Tipo colecciones -> True para todas las demas
+
+# Lista
+valor = []
+resultado = bool(valor)
+print(f"valor de una lista vacia: {valor}, Resultado: {resultado}")
+
+valor = [1, 2, 3]
+resultado = bool(valor)
+print(f"valor de una lista con elementos: {valor}, Resultado: {resultado}")
+
+# Tupla
+valor = ()
+resultado = bool(valor)
+print(f"valor de una tupla vacia: {valor}, Resultado: {resultado}")
+
+valor = (1, )
+resultado = bool(valor)
+print(f"valor de una tupla con elementos: {valor}, Resultado: {resultado}")
+
+# Diccionario
+valor = {}
+resultado = bool(valor)
+print(f"valor de un diccionario vacio: {valor}, Resultado: {resultado}")
+
+valor = {"Nombre": "Franco", "Apellido": "Pagano"}
+resultado = bool(valor)
+print(f"valor de un diccionario con elementos: {valor}, Resultado: {resultado}")
