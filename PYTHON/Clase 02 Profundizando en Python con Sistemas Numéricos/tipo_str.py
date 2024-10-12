03ebedbe557ae961d6c77987218c1ecada636995
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-10-12 20:08:28 -0300

    Concatenación de String

---
+++ PYTHON/Clase 02 Profundizando en Python con Sistemas Numéricos/tipo_str.py
@@ -0,0 +1,8 @@
+# Profucndizando en el tipo String
+# Concatenacion automatica en Python
+
+variable = " Pagano"
+mensaje = "Hola " + "Franco" # El sigo de mas es innecesario usarlo siempre y cuando sean los dos string o esten estre ""
+mensaje = "Hola ""Franco" + variable # en este caso a juntar con una variable si se utiliza el conectar
+mensaje += ", Terminamos"
+print(mensaje)
