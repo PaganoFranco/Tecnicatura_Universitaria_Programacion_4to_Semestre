9878cb94aa73995132782f76f8ba87ac05993ce3
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-11-12 01:38:11 -0300

    Clase 5 - 6 Python

---
+++ PYTHON/Clase 02 Profundizando en Python con Sistemas Numéricos/doc_str.py
@@ -0,0 +1,8 @@
+from mi_clase import MiClase
+
+# help(MiClase) #ASI ACCEDEMOS A LA DOCUMENTACION DE LA CLASE
+# print(MiClase.__doc__) #Muestra la ducumentacion que creamos en la clase
+# print(MiClase.__init__.__doc__)
+# print(MiClase.mi_metodo.__doc__)
+print(MiClase.mi_metodo)
+print(type(MiClase.mi_metodo))
---
+++ PYTHON/Clase 02 Profundizando en Python con Sistemas Numéricos/mi_clase.py
@@ -0,0 +1,23 @@
+class MiClase:
+    """
+    Ejemplo de una clase llamada MiClase.
+    Esta clase se utiliza como referencia para mostrar cómo documentar correctamente
+    una clase en Python.
+    """
+    def __init__(self):
+        """
+        Método constructor de la clase MiClase.
+        Se utiliza para inicializar cualquier atributo que sea necesario al crear
+        una instancia de la clase.
+        """
+
+    def mi_metodo(self, param1, param2):
+        """
+        Método de ejemplo que recibe dos parámetros.
+        Este método está diseñado para mostrar cómo documentar los parámetros y
+        el valor de retorno de un método.
+
+        :param param1: Primer parámetro del método, se espera un valor determinado según el uso.
+        :param param2: Segundo parámetro del método, se espera un valor complementario.
+        :return: Devuelve un valor (especificar el tipo o propósito del retorno si es relevante).
+        """
