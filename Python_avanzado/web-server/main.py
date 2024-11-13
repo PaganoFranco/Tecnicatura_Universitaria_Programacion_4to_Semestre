0bd57ec329b3dca11a81674562ff0a546e28b749
Franco Pagano <135770704+PaganoFranco@users.noreply.github.com>
2024-11-13 19:24:20 -0300

    Clase 12 Python en Entorno Profesional -> Parte 7

--- Python_avanzado/web-server/main.py
+++ Python_avanzado/web-server/main.py
@@ -1,4 +1,22 @@
 import store
+from frasapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI() # Creamos nuestra primera instancia y con estem cremos nuestro primer recurso
+
+#Ruta principal
+@app.get('/') # Agregmos el decorador para decirle cual es la ruta
+def grt_list(): # Va a devolver una lista
+    return[1, 2, 3]
+
+# Ruta secundaria
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1> Soy un sitio web </h1>
+        <p> Soy un parrafo para que tu leas </p>
+    """
+
 def run():
     store.get_razas()
 
